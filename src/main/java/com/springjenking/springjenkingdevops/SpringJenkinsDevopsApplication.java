01153f8181dd7802b047febca1eacdfab6657f8f
Asif Ahmed <asif3021@diu.edu.bd>
2024-01-19 21:38:10 +0600

    Build failed

--- src/main/java/com/springjenking/springjenkingdevops/SpringJenkinsDevopsApplication.java
+++ src/main/java/com/springjenking/springjenkingdevops/SpringJenkinsDevopsApplication.java
@@ -12,7 +12,7 @@ public class SpringJenkinsDevopsApplication {
 	static Logger logger = LoggerFactory.getLogger(SpringJenkinsDevopsApplication.class);
 
 	@PostConstruct
-	public void init() {
+	public  init() {
 		logger.info("Hello world...");
 		logger.info("Application started...");
 	}
